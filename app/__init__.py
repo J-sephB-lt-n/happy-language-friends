458b3a203ead5678a6bfba8e19db18c62d5c14b2
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-07-13 23:07:38 +0200

    feat(happy_rag_friends/): package working

--- app/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Steps taken at app initialisation"""
-
-import flask
-
-flask_app = flask.Flask(__name__)
-
-from app import routes
--- app/endpoints/home.py
+++
@@ -1,7 +0,0 @@
-"""Page shown on app entry"""
-
-import flask
-
-
-def home_page():
-    return flask.Response("Hello World", status=200)
