a330c59864463501b07650075e4715e8fdadb033
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-07-18 20:41:16 +0200

    feat(__init__.py-setup.py): Resolved package data plan

--- happy_rag_friends/__init__.py
+++ happy_rag_friends/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 
-from happy_rag_friends import routes
+from happy_rag_friends import routes, setup
 
 
 def create_app():
     app = Flask(__name__)
+    setup.create_package_database_if_not_exists()
     app.register_blueprint(routes.bp)
     return app
