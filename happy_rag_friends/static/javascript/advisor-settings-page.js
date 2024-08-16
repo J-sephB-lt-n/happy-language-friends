62f18369735c9942e0b4a1ec16ff745a48fcad14
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-08-16 21:55:12 +0200

    fix(javascript/advisor-settings-page.js): advisor select list populates on page load

--- happy_rag_friends/static/javascript/advisor-settings-page.js
+++ happy_rag_friends/static/javascript/advisor-settings-page.js
@@ -20,13 +20,19 @@ function updateAdvisorSelectList() {
   const selectAdvisor = document.getElementById("select-advisor");
   selectAdvisor.options.length = 0; // clear list
 
-  advisorsData = getAdvisorsData();
-  advisorsData.forEach((advisor) => {
-    const option = document.createElement("option");
-    option.value = advisor.advisor_name;
-    option.textContent = advisor.advisor_name;
-    selectAdvisor.appendChild(option);
-  });
+  getAdvisorsData()
+    .then((advisorsData) => {
+      Object.values(advisorsData).forEach((advisor) => {
+        const option = document.createElement("option");
+        option.value = advisor.advisor_name;
+        option.textContent = advisor.advisor_name;
+        selectAdvisor.appendChild(option);
+      });
+    })
+    .catch((error) => {
+      alert(`Error updating advisor select list`);
+      console.error("Error updating advisor select list:", error);
+    });
   const option = document.createElement("option");
   option.value = "create-new-advisor";
   option.textContent = "[+] Create New Advisor";
@@ -78,73 +84,74 @@ function updateElementsOnAdvisorSelect() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+  updateAdvisorSelectList();
   updateLLMSelectList();
-  fetch("http://127.0.0.1:5000/backend/get_advisor_details")
-    .then((response) => response.json())
-    .then((data) => {
-      const selectAdvisor = document.getElementById("select-advisor");
-
-      // Remove the "create-new-advisor" option temporarily
-      const createNewAdvisorOption = selectAdvisor.querySelector(
-        'option[value="create-new-advisor"]',
-      );
-      selectAdvisor.removeChild(createNewAdvisorOption);
-
-      data.forEach((advisor) => {
-        const option = document.createElement("option");
-        option.value = advisor.advisor_name;
-        option.textContent = advisor.advisor_name;
-        selectAdvisor.appendChild(option);
-      });
-
-      selectAdvisor.appendChild(createNewAdvisorOption); // Add the "create-new-advisor" option back to the end of the select-advisor dropdown
-      selectAdvisor.selectedIndex = 0;
-
-      // Store advisor data for later use
-      const advisorsData = data.reduce((acc, advisor) => {
-        acc[advisor.advisor_name] = advisor;
-        return acc;
-      }, {});
-
-      // Update elements when a new advisor is selected
-      var advisorNameInput = document.getElementById("advisor-name");
-      var advisorPersonalityInput = document.getElementById(
-        "advisor-personality",
-      );
-      var advisorModelPath = document.getElementById("model-path");
-      function updateInputBoxContents() {
-        var selectedAdvisor = selectAdvisor.value;
-        const applyChangesButton = document.getElementById("apply-changes");
-        if (advisorsData[selectedAdvisor]) {
-          advisorNameInput.value = selectedAdvisor;
-          advisorPersonalityInput.value =
-            advisorsData[selectedAdvisor].personality_description;
-          advisorModelPath.value = advisorsData[selectedAdvisor].path_to_model;
-          applyChangesButton.value = "Apply Changes";
-        } else {
-          advisorNameInput.value = "<advisor name here>";
-          advisorPersonalityInput.value =
-            "<advisor personality description here>";
-          advisorModelPath.value = "/specify/path/to/your/model/here.gguf";
-          applyChangesButton.value = "Create New Advisor";
-        }
-      }
-
-      selectAdvisor.addEventListener("change", updateInputBoxContents); // Update advisor-name and advisor-personality input boxes when different advisor is selected
-      updateInputBoxContents(); // update input boxes on page load
-    })
-    .catch((error) => console.error("Error fetching advisor details:", error));
+  // fetch("http://127.0.0.1:5000/backend/get_advisor_details")
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     const selectAdvisor = document.getElementById("select-advisor");
+  //
+  //     // Remove the "create-new-advisor" option temporarily
+  //     const createNewAdvisorOption = selectAdvisor.querySelector(
+  //       'option[value="create-new-advisor"]',
+  //     );
+  //     selectAdvisor.removeChild(createNewAdvisorOption);
+  //
+  //     data.forEach((advisor) => {
+  //       const option = document.createElement("option");
+  //       option.value = advisor.advisor_name;
+  //       option.textContent = advisor.advisor_name;
+  //       selectAdvisor.appendChild(option);
+  //     });
+  //
+  //     selectAdvisor.appendChild(createNewAdvisorOption); // Add the "create-new-advisor" option back to the end of the select-advisor dropdown
+  //     selectAdvisor.selectedIndex = 0;
+  //
+  //     // Store advisor data for later use
+  //     const advisorsData = data.reduce((acc, advisor) => {
+  //       acc[advisor.advisor_name] = advisor;
+  //       return acc;
+  //     }, {});
+  //
+  //     // Update elements when a new advisor is selected
+  //     var advisorNameInput = document.getElementById("advisor-name");
+  //     var advisorPersonalityInput = document.getElementById(
+  //       "advisor-personality",
+  //     );
+  //     var advisorModelPath = document.getElementById("model-path");
+  //     function updateInputBoxContents() {
+  //       var selectedAdvisor = selectAdvisor.value;
+  //       const applyChangesButton = document.getElementById("apply-changes");
+  //       if (advisorsData[selectedAdvisor]) {
+  //         advisorNameInput.value = selectedAdvisor;
+  //         advisorPersonalityInput.value =
+  //           advisorsData[selectedAdvisor].personality_description;
+  //         advisorModelPath.value = advisorsData[selectedAdvisor].path_to_model;
+  //         applyChangesButton.value = "Apply Changes";
+  //       } else {
+  //         advisorNameInput.value = "<advisor name here>";
+  //         advisorPersonalityInput.value =
+  //           "<advisor personality description here>";
+  //         advisorModelPath.value = "/specify/path/to/your/model/here.gguf";
+  //         applyChangesButton.value = "Create New Advisor";
+  //       }
+  //     }
+  //
+  //     selectAdvisor.addEventListener("change", updateInputBoxContents); // Update advisor-name and advisor-personality input boxes when different advisor is selected
+  //     updateInputBoxContents(); // update input boxes on page load
+  //   })
+  // .catch((error) => console.error("Error fetching advisor details:", error));
 
   // make delete advisor button disappear whenever "create-new-advisor" is selected
-  const selectAdvisor = document.getElementById("select-advisor");
-  const deleteAdvisorForm = document.getElementById("delete-advisor");
-  selectAdvisor.addEventListener("change", function () {
-    if (selectAdvisor.value === "create-new-advisor") {
-      deleteAdvisorForm.style.display = "none";
-    } else {
-      deleteAdvisorForm.style.display = "inline";
-    }
-  });
+  // const selectAdvisor = document.getElementById("select-advisor");
+  // const deleteAdvisorForm = document.getElementById("delete-advisor");
+  // selectAdvisor.addEventListener("change", function () {
+  //   if (selectAdvisor.value === "create-new-advisor") {
+  //     deleteAdvisorForm.style.display = "none";
+  //   } else {
+  //     deleteAdvisorForm.style.display = "inline";
+  //   }
+  // });
 });
 
 function checkFilePath() {
